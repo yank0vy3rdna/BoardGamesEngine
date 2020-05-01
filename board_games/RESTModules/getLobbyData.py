0ca3adbb847a09f8eb4388c5afc1e52ece0b6430
yank0vy3rdna <yankovyerdna@yandex.ru>
2020-05-01 15:40:23 +0300

    getLobbyData module

--- board_games/RESTModules/getLobbyData.py
+++ board_games/RESTModules/getLobbyData.py
@@ -2,8 +2,7 @@ from board_games.DataStorage import *
 
 
 def getLobbyData_module(lobby_id: int):
-    # if lobby_exists(lobby_id):
-    #     return get_lobby(lobby_id)
-    # else:
-    #     return "{'status': 1}"
-    return "Не допилено"
+    if isExists(lobby_id):
+        return get(lobby_id)
+    else:
+        return "{'status': 1}"
