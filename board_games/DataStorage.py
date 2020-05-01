03c59488286dcd5dea356004d31e7df1b32bd6df
yank0vy3rdna <yankovyerdna@yandex.ru>
2020-05-01 13:34:41 +0300

    Error fixed

--- board_games/DataStorage.py
+++ board_games/DataStorage.py
@@ -1,5 +1,7 @@
 import sqlite3 as sql
-conn = sql.connect("Database.db")
+import os
+
+conn = sql.connect("content" + os.path.sep + "Database.db")
 
 cursor = conn.cursor()
 lobbies = dict()
@@ -16,24 +18,28 @@ cursor.execute('CREATE TABLE IF NOT EXISTS players('
                )
 conn.commit()
 
+
 class Lobby:
     patch = ""
-    #lobby_id, game, players, parameters = None
+
+    # lobby_id, game, players, parameters = None
     def __init__(self, game, players, parameters):
+        global free_lobby, cursor
         self.game = game
         self.players = players
         self.parameters = parameters
         self.lobby_id = free_lobby
         free_lobby += 1
         cursor.execute('INSERT INTO lobbies (id, name) VALUES (?, ?)', (self.lobby_id, self.game['name']))
-        cursor.commit()
+        conn.commit()
         cursor.execute('UPDATE players SET game = ? WHERE id IN ?', (self.lobby_id, self.players))
-        cursor.commit()
-        lobbies[lobby_id] = self
+        conn.commit()
+        lobbies[self.lobby_id] = self
+
     def __del__(self):
         lobbies[self.lobby_id] = None
-        cursor.execute('DELETE FROM lobbies WHERE id = ?', (self.lobby_id))
-        cursor.commit()
-        cursor.execute('UPDATE players SET game = -1 WHERE id = ?', (self.lobby_id))
-        cursor.commit()
-        super(Lobby, self).__del__()
+        cursor.execute('DELETE FROM lobbies WHERE id = ?', [self.lobby_id])
+        conn.commit()
+        cursor.execute('UPDATE players SET game = -1 WHERE id = ?', [self.lobby_id])
+        conn.commit()
+        # super(Lobby, self).__del__()
--- board_games/main.py
+++ board_games/main.py
@@ -1,6 +1,11 @@
 from board_games.game import GameLoader, GameInterpreter
+from board_games.DataStorage import *
+
 game_loader = GameLoader.GameLoader("/home/yank0vy3rdna/projects/AllSite/board_games/content/game.zip")
 game = game_loader.get_game()
-GameInterpreter.GameInterpreter().interprete("a = 0", game)
-
+game['name']='prikol'
 print(game)
+Lobby(game, [54], {'max_players_count': 4})
+
+# GameInterpreter.GameInterpreter().interprete("a = 0", game)
+
