6c55c16a03aff35635e5c1cb66b4b876d36b681c
alexgavrilov28@gmail.com <alexgavrilov28@gmail.com>
2020-05-01 19:23:15 +0300

    add exceptions

--- board_games/RESTModules/createLobby.py
+++ board_games/RESTModules/createLobby.py
@@ -7,14 +7,17 @@ import board_games.DataStorage as ds
 import board_games.game.GameLoader as gl
 app = Flask(__name__)
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
-@app.route('/board_games/CreateLobby', methods=['POST'])
+@app.route('/board_games/createLobby', methods=['POST'])
 def createLobby():
     file = base64.b64decode(request.files['file']).decode("UTF-8")
     filename = secure_filename(file.filename)
-    lobby_id = ds.free_lobby
-    lobby = gl.GameLoader(file, lobby_id)
-    ds.Lobby(lobby.game, [], [])
-    path = "board_games" + os.path.sep + "lobbies" + os.path.sep + "lobby_" + lobby_id + os.path.sep + "file.zip"
-    file.save(os.path.join(path))
-    return json.encoder(lobby_id)
+    try:
+        lobby_id = ds.free_lobby
+        lobby = gl.GameLoader(file, lobby_id)
+        ds.Lobby(lobby.game, [], [])
+        path = "board_games" + os.path.sep + "lobbies" + os.path.sep + "lobby_" + lobby_id + os.path.sep + "file.zip"
+        file.save(os.path.join(path))
+        return json.encoder(lobby_id)
+    except:
+        pass
 
